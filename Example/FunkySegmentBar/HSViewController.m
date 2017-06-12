fc09a10ba268f62fe04bf9c2f7956a5ac33264cb
funkyHS <hs1024942667@163.com>
2017-06-13 00:30:28 +0800

    选项卡组件完成

--- Example/FunkySegmentBar/HSViewController.m
+++ Example/FunkySegmentBar/HSViewController.m
@@ -7,23 +7,81 @@
 //
 
 #import "HSViewController.h"
+#import "HSSementBarVC.h"
+
 
 @interface HSViewController ()
+
+@property (nonatomic, weak) HSSementBarVC *segmentBarVC;
+
 
 @end
 
 @implementation HSViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    
+    self.segmentBarVC.segmentBar.frame = CGRectMake(0, 0, 300, 35);
+////    self.segmentBarVC.segmentBar.backgroundColor = [UIColor greenColor];
+    self.navigationItem.titleView = self.segmentBarVC.segmentBar;
+//
+    self.segmentBarVC.view.frame = self.view.bounds;
+    [self.view addSubview:self.segmentBarVC.view];
+//    
+    
+    NSArray *items = @[@"专辑", @"声音", @"下载中"];
+    
+    // 添加几个自控制器
+    // 在contentView, 展示子控制器的视图内容
+    
+    UIViewController *vc1 = [UIViewController new];
+    vc1.view.backgroundColor = [UIColor redColor];
+    
+    UIViewController *vc2 = [UIViewController new];
+    vc2.view.backgroundColor = [UIColor greenColor];
+    
+    UIViewController *vc3 = [UIViewController new];
+    vc3.view.backgroundColor = [UIColor yellowColor];
+    
+    
+    [self.segmentBarVC setUpWithItems:items childVCs:@[vc1, vc2, vc3]];
+    
+//    [self.segmentBarVC.segmentBar updateWithConfig:^(HSSegmentBarConfig *config) {
+//        
+//        config.segmentBarBackColor = [UIColor cyanColor];
+//        //            config.itemNormalColor = [UIColor brownColor];
+//        //            config.itemSelectColor = [UIColor yellowColor];
+//        config.itemSC([UIColor brownColor]).itemNC([UIColor yellowColor]);
+//        config.itemFont = [UIFont fontWithName:@"Zapfino" size:10];
+//        
+//        config.indicatorHeight = 5;
+//        config.indicatorColor = [UIColor blueColor];
+//        config.indicatorExtraW = 0;
+//        
+//    }];
+    
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+
+#pragma mark - 懒加载
+
+- (HSSementBarVC *)segmentBarVC {
+    if (!_segmentBarVC) {
+        HSSementBarVC *vc = [[HSSementBarVC alloc] init];
+        [self addChildViewController:vc];
+        _segmentBarVC = vc;
+    }
+    return _segmentBarVC;
 }
 
 @end
