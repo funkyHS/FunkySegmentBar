8d11f37b5ace8303964f99b397b45d1f198490b4
funkyHS <hs1024942667@163.com>
2017-06-13 14:37:54 +0800

    提交选项卡到pods中

---
+++ Example/Pods/Target Support Files/FunkyFMBase/FunkyFMBase-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CALayer+PauseAimate.h"
+#import "NSObject+HSBundle.h"
+#import "UIImage+HSImage.h"
+#import "UIView+HSLayout.h"
+#import "UIView+HSNib.h"
+
+FOUNDATION_EXPORT double FunkyFMBaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char FunkyFMBaseVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-FunkySegmentBar_Tests/Pods-FunkySegmentBar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FunkySegmentBar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FunkySegmentBar_Tests
+@end
